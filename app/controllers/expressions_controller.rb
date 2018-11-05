3147062e743116e7ad15fbb6ec1912af9e92db93
darinnelson <dnelson5614@gmail.com>
2018-11-04 20:38:27 -0600

    Courses have access to expressions

--- app/controllers/expressions_controller.rb
+++ app/controllers/expressions_controller.rb
@@ -1,6 +1,6 @@
 class ExpressionsController < ApplicationController
   def index
-#    @expressions = Expression.all
-#    render "index.json.jbuilder"
+   @expressions = Expression.all
+   render "index.html.erb"
   end
 end
---
+++ app/models/expression.rb
@@ -0,0 +1,3 @@
+class Expression < ApplicationRecord
+  belongs_to :course
+end
